faf2391e93a00453c62232bf4e9ad45a9cb67bde
WaterMax <grandmasterm@gmx.de>
2017-12-16 14:30:53 +0100

    fix for changing games

--- StFaeKSC/Data/games.cpp
+++ StFaeKSC/Data/games.cpp
@@ -298,9 +298,10 @@ GamesPlay* Games::gameExists(quint8 sIndex, CompetitionIndex comp, quint16 saiso
         if (pGame == NULL)
             continue;
         if (pGame->m_saisonIndex == sIndex && pGame->m_competition == comp && pGame->m_saison == saison) {
-            QDateTime oldData = QDateTime::fromMSecsSinceEpoch(pGame->m_timestamp);
+//            QDateTime oldData = QDateTime::fromMSecsSinceEpoch(pGame->m_timestamp);
             //if (date.date().year() == oldData.date().year())
-            //    return pGame;
+                return pGame;
+
         }
         /* Game also exists when it is the exact timestamp, to change wrong competition or seasonIndex */
         if (pGame->m_timestamp == timestamp && (pGame->m_competition == comp || pGame->m_saisonIndex == sIndex))
